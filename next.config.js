8c1d33fb92a2b7270af06d7ddae7a8ccf1f90192
ihsan <ihksansanhas@gmail.com>
2021-11-08 12:28:54 +0800

    fix: bugfix error middleware manifest json

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
-    runtimeCaching
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
   },
 })
