406cfe7ceb52d0fc1d61361d20ef580339a18394
ihsan <ihksansanhas@gmail.com>
2021-11-08 11:35:21 +0800

    feat: bugfix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>{"Sobat Qur'an"}</title>
+          <meta charSet="utf-8" />
+          <meta name="og:description" content="Baca Qur'an Online dengan Sobat Qur'an" />
+          <meta name="og:keywords" content="Sobat Quran, Baca Qur'an Online" />
+          <meta property="og:image" content="/icons/icon-512x512.png" />  
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="https://fonts.googleapis.com/css2?family=Scheherazade:wght@400;700&display=swap" rel="stylesheet" />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" 
+            rel="stylesheet" 
+          />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
         <body>
           <Main />
           <NextScript />
