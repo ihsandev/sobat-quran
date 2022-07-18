53c9d70a9b10f5ca79c074c0c9e008c60843b98a
ihsan <ihksansanhas@gmail.com>
2022-07-18 11:31:58 +0700

    handle when empty bookmark

--- apps/containers/Quran/partials/LastAyat.tsx
+++ apps/containers/Quran/partials/LastAyat.tsx
@@ -8,32 +8,35 @@ const LastAyat = () => {
   const { state } = useAppContext();
   const { push } = useRouter();
   const lastRead = state.bookmark?.slice(-1);
-  return (
-    <Flex
-      mx="1.2rem"
-      flex={1}
-      my="1rem"
-      cursor="pointer"
-      bgColor="primary"
-      color="soft"
-      px="1rem"
-      py="0.7rem"
-      borderRadius="1rem"
-      boxSizing="border-box"
-      onClick={() => {
-        const idAyat = `ayat-${lastRead[0]?.number.inSurah}-${lastRead[0]?.numberId}`;
-        addToLocalStorage("id-ayat", idAyat);
-        push(`/surah/${lastRead[0]?.numberId}`);
-      }}
-    >
-      <Box flex={1}>
-        <Heading fontSize="1rem">Terakhir Baca</Heading>
-        <Text fontSize="0.9rem">{`Surah ${lastRead[0]?.name}, Ayat ke-${lastRead[0]?.number?.inSurah}`}</Text>
-        <Text fontSize="0.9rem">{`di Juz ke-${lastRead[0]?.meta?.juz}`}</Text>
-      </Box>
-      <FiArrowRight size={20} />
-    </Flex>
-  );
+  if (state.bookmark?.length) {
+    return (
+      <Flex
+        mx="1.2rem"
+        flex={1}
+        my="1rem"
+        cursor="pointer"
+        bgColor="primary"
+        color="soft"
+        px="1rem"
+        py="0.7rem"
+        borderRadius="1rem"
+        boxSizing="border-box"
+        onClick={() => {
+          const idAyat = `ayat-${lastRead[0]?.number.inSurah}-${lastRead[0]?.numberId}`;
+          addToLocalStorage("id-ayat", idAyat);
+          push(`/surah/${lastRead[0]?.numberId}`);
+        }}
+      >
+        <Box flex={1}>
+          <Heading fontSize="1rem">Terakhir Baca</Heading>
+          <Text fontSize="0.9rem">{`Surah ${lastRead[0]?.name}, Ayat ke-${lastRead[0]?.number?.inSurah}`}</Text>
+          <Text fontSize="0.9rem">{`di Juz ke-${lastRead[0]?.meta?.juz}`}</Text>
+        </Box>
+        <FiArrowRight size={20} />
+      </Flex>
+    );
+  }
+  return null;
 };
 
 export default LastAyat;
